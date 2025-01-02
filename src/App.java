4d0b2632484ac6b2cfd8fa58ac3f9925c85c4f3f
Arish <arishchowdhury.2021@gmail.com>
2025-01-02 23:20:03 +0600

    i made 1d array

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // This is a 1d array
+        int myarray[] = {1,2,3,4,5};
+        System.out.println(myarray[2]);
+
+
+        // for loop to print EVERYTHING from array using index
+        for (int i = 0; i < myarray.length; i++){
+            System.out.println(myarray[i]);
+
+        }
+        
+
+    }
+}

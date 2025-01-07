cef08aafc68008cbc4a530e5818fa9ad6f2d85f8
Arish <arishchowdhury.2021@gmail.com>
2025-01-07 23:20:03 +0600

    made 3d array and declared and initalized arrays

--- src/App.java
+++ src/App.java
@@ -4,12 +4,39 @@ public class App {
         int myarray[] = {1,2,3,4,5};
         System.out.println(myarray[2]);
 
-
         // for loop to print EVERYTHING from array using index
         for (int i = 0; i < myarray.length; i++){
             System.out.println(myarray[i]);
 
         }
+
+        int myaraaay2[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(myaraaay2[1][2]);
+        System.out.println(myaraaay2[2][1]);
+
+
+        int myarray3[][][] = {{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}},{{13,14,15},{16,17,18}}};
+        System.out.println(myarray3[1][0][1]);
+
+        //declaring the array with 6 items
+
+        int array4[] = new int[6];
+        
+        // intializing the array
+        array4[0] = 1;
+        array4[1] = 2;
+        array4[2] = 3;
+        array4[3] = 4;
+        array4[4] = 5;
+        array4[5] = 6;
+
+        for (int i = 0; i<array4.length; i++){
+            System.out.println(array4[i]);
+            
+        }
+
+
+        
         
 
     }
